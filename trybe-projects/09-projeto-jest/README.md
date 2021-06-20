c4a22549220674098ba5758fccd18830fa910608
Renata Nunes <renatanunes231@yahoo.com.br>
2021-06-20 18:32:32 -0300

    Projeto Jest adicionado

---
+++ trybe-projects/09-projeto-jest/README.md
@@ -0,0 +1,14 @@
+# Projeto: Jest
+
+_Projeto proposto no curso de Desenvolvimento Web da_ [_Trybe_](https://www.betrybe.com/)
+
+##### Proposta:
+-   Escrever testes para funções assíncronas;
+-   Aplicar os seus conhecimentos acerca de testes utilizando o Jest;
+-   "Mockar" funções;
+-   "Mockar" APIs;
+
+##### O que foi desenvolvido:
+Foram implementados testes utilizando o **Jest** para verificar uma série de funções que já estavam funcionando corretamente. 
+
+Para garantir a qualidade do código de forma a tê-lo mais legível, de fácil manutenção e seguindo as boas práticas de desenvolvimento, foi utilizado neste projeto o [ESLint](https://eslint.org/).
---
+++ trybe-projects/09-projeto-jest/test/setupTeardown.spec.js
@@ -0,0 +1,53 @@
+const adventure = require('../src/setupTeardown');
+/*
+Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
+O grupo parte em direção ao sucesso, mas,
+devido a ameaça de criaturas temíveis, o grupo não chegará inteiro ao fim.
+Após cada aventura um de nossos aventureiros cairá.
+Cada um dos testes abaixo verifica a quantidade de aventureiros após cada iteração.
+Sua missão aqui é:
+
+  - Use a função randomAttack do objeto adventure
+  que remove um dos aventureiros toda vez que é chamada,
+  ela deve funcionar entre cada teste.
+  Opcional:
+  - Para ficar mais visível, imprima na tela após cada teste o grupo de aventureiros restante.
+  - No fim dos testes, imprima uma mensagem com o nome do aventureiro que sobreviveu.
+
+PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
+
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+*/
+
+describe('quem sobreviveu?', () => {
+  // Adicione seu código aqui
+
+  test('depois da primeira aventura', () => {
+    adventure.randomAttack();
+    expect(adventure.specialists.length).toBe(5);
+    console.log(adventure.specialists);
+  });
+  test('depois da segunda aventura', () => {
+    adventure.randomAttack();
+    expect(adventure.specialists.length).toBe(4);
+    console.log(adventure.specialists);
+  });
+  test('depois da terceira aventura', () => {
+    adventure.randomAttack();
+    expect(adventure.specialists.length).toBe(3);
+    console.log(adventure.specialists);
+  });
+  test('depois da quarta aventura', () => {
+    adventure.randomAttack();
+    expect(adventure.specialists.length).toBe(2);
+    console.log(adventure.specialists);
+  });
+  test('depois da quinta aventura', () => {
+    adventure.randomAttack();
+    expect(adventure.specialists.length).toBe(1);
+    console.log(adventure.specialists);
+    console.log(`${adventure.specialists[0].nome},
+  o ${adventure.specialists[0].classe},
+  foi o bravo aventureiro sobrevivente!`);
+  });
+});
